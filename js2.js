0a95d610e1792fd5336fd1ee700a8374a118c242
miley72 <98891285+miley72@users.noreply.github.com>
2022-06-24 20:51:05 +0100

    Create js2.js

---
+++ js2.js
@@ -0,0 +1,12 @@
+var name = "Mileyyyy";
+
+alert(name)
+
+
+var height = "6.7m";
+alert(height)
+
+var country = "Nigeria";
+alert(country)
+
+alert(`Hello, I'm ${name}, I'm ${height} inch tall, and I'm from ${country}`)
